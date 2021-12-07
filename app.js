79fcf2e610d7ba9dfcfbfc8965e407f2f59a5f15
JCGG <jcglez98@gmail.com>
2021-12-06 19:55:45 -0600

    Fix CORS

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ require('./passport-config')(passport);
 
 const port = process.env.PORT || 3001;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 app.use('/assets', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -43,26 +48,6 @@ app.use(passport.session());
 
 app.get('/', function(req, res) {
   res.send('Application works!');
-});
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
 });
 
 app.use(router);
